2fc4e6a78dfcaf046d783594bca4172b5653790e
senaydaniel <daniel.304@osu.edu>
2017-02-06 21:21:48 -0500

    Lab 2 Complete!

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+[Route("api/auto/quotes")]
+
+    public class HomeController : Controller();
+    {
+       //POST api/auto/quotes
+
+       [HttpPost] 
+
+       public IActionResult Post([FromBody] string value)
+
+       {
+           return Created("", value);
+       }
+
+       //Get api/auto/quotes/5
+
+            [HttpGet("{id}")]
+
+            public IActionResult Get(int id)
+            {
+                return Ok("The id is: " + id);
+            }
+
+            //PUT api/auto/quotes/id
+
+            [HttpPut("{id}")]
+
+            public IActionResult Put(int id, [FromBody]string value)
+
+            {
+                return NoContent();
+            }
+
+            // DELETE api/auto/quotes/id
+             [HttpDelete("{id}")]
+
+             public IActionResult Delete(int id)
+
+             {
+                 return Delete(id);
+             }
+
+        
+
+    }
